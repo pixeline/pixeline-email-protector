bf9003908c9b35b30120d08e2f8be47ff6500031
Plennevaux Alexandre <aplennevaux@gmail.com>
2017-01-06 01:25:08 +0100

    bump to 1.3.5

--- deploy.sh
+++ deploy.sh
@@ -32,29 +32,15 @@ echo
 echo ".........................................."
 echo 
 
-# Check if subversion is installed before getting all worked up
-if ! which svn >/dev/null; then
-	echo "You'll need to install subversion before proceeding. Exiting....";
-	exit 1;
-fi
-
-# Check version in readme.txt is the same as plugin file after translating both to unix line breaks to work around grep's failure to identify mac line breaks
-NEWVERSION1=`grep "^Stable tag:" $GITPATH/readme.txt | awk -F' ' '{print $NF}'`
-echo "readme.txt version: $NEWVERSION1"
-NEWVERSION2=`grep "^Version:" $GITPATH/$MAINFILE | awk -F' ' '{print $NF}'`
+# Check version in readme.txt is the same as plugin file
+NEWVERSION1=`grep "^Stable tag" $GITPATH/readme.txt | awk -F' ' '{print $3}'`
+echo "readme version: $NEWVERSION1"
+NEWVERSION2=`grep "^Version" $GITPATH/$MAINFILE | awk -F' ' '{print $2}'`
 echo "$MAINFILE version: $NEWVERSION2"
 
-if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then echo "Version in readme.txt & $MAINFILE don't match. Exiting...."; exit 1; fi
+if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then echo "Versions don't match. Exiting...."; exit 1; fi
 
-echo "Versions match in readme.txt and $MAINFILE. Let's proceed..."
-
-if git show-ref --tags --quiet --verify -- "refs/tags/$NEWVERSION1"
-	then 
-		echo "Version $NEWVERSION1 already exists as git tag. Exiting...."; 
-		exit 1; 
-	else
-		echo "Git version does not exist. Let's proceed..."
-fi
+echo "Versions match in readme.txt and PHP file. Let's proceed..."
 
 cd $GITPATH
 echo -e "Enter a commit message for this new version: \c"
@@ -72,18 +58,14 @@ echo
 echo "Creating local copy of SVN repo ..."
 svn co $SVNURL $SVNPATH
 
-echo "Clearing svn repo so we can overwrite it"
-svn rm $SVNPATH/trunk/*
-
 echo "Exporting the HEAD of master from git to the trunk of SVN"
 git checkout-index -a -f --prefix=$SVNPATH/trunk/
 
-echo "Ignoring github specific files and deployment script"
+echo "Ignoring github specific & deployment script"
 svn propset svn:ignore "deploy.sh
 README.md
 .git
 .gitignore" "$SVNPATH/trunk/"
-
 
 echo "Moving assets-wp-repo"
 mkdir $SVNPATH/assets/
@@ -92,18 +74,16 @@ svn add $SVNPATH/assets/
 svn delete $SVNPATH/trunk/assets-wp-repo
 
 
-echo "Changing directory to SVN and committing to trunk"
+echo "Changing directory to SVN"
 cd $SVNPATH/trunk/
 # Add all new files that are not set to be ignored
 svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
-
 echo "committing to trunk"
 svn commit --username=$SVNUSER -m "$COMMITMSG"
 
 echo "Updating WP plugin repo assets & committing"
 cd $SVNPATH/assets/
 svn commit --username=$SVNUSER -m "Updating wp-repo-assets"
-
 
 echo "Creating new SVN tag & committing it"
 cd $SVNPATH
